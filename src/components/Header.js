627d8db4566cfd10c90a11f1f8945598d7d88027
Sergei Kompanietc <delightvlg@gmail.com>
2021-01-10 22:05:48 +0300

    added all reg and log logic. added tooltip modal.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 
-const Header = () => (
+const Header = ({ handleLogout, email }) => (
   <header className="header page__header">
     <div className="header__logo" />
+    <Switch>
+      <Route exact path="/">
+        <div className="header__wrapper">
+          <p className="header__email">{email}</p>
+          <Link
+            to="/sign-in"
+            onClick={handleLogout}
+            className="header__link"
+          >Выйти</Link>
+        </div>
+      </Route>
+
+      <Route path="/sign-in">
+        <Link to="/sign-up" className="header__link">Зарегистрироваться</Link>
+      </Route>
+
+      <Route path="/sign-up">
+        <Link to="/sign-in" className="header__link">Войти</Link>
+      </Route>
+    </Switch>
   </header>
 );
 
